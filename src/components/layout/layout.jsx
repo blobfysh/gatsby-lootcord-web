d8cba29ed0a34d31439732d704998b127538c05e
blobfysh <47480729+blobfysh@users.noreply.github.com>
2020-11-08 11:45:53 -0500

    remove useTheme.jsx

--- src/components/layout/layout.jsx
+++ src/components/layout/layout.jsx
@@ -4,16 +4,25 @@ import '../../styles/main.scss'
 // prismjs code styling
 import 'prismjs/themes/prism.css'
 
-import React from 'react'
+import React, { useState, useLayoutEffect } from 'react'
 import ThemeContext from '../../context/ThemeContext'
 
-import useTheme from '../useTheme'
 import Header from '../header/header'
 import Footer from '../footer/footer'
 import styles from './layout.module.scss'
 
 function Layout({ children }) {
-	const theme = useTheme()
+	const [theme, setTheme] = useState(null)
+
+	// sets theme on component mount
+	// useLayoutEffect updates before browser paints whereas useEffect runs after component is rendered
+	useLayoutEffect(() => {
+		setTheme(window.theme)
+
+		window.onThemeChange = () => {
+			setTheme(window.theme)
+		}
+	}, [])
 
 	return (
 		<div className={styles.contentFull}>
