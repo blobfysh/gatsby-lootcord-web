6eb1fa1c816991f6d6a912380e796158af6ccefa
blobfysh <47480729+blobfysh@users.noreply.github.com>
2020-11-10 19:21:20 -0500

    create Command and Patron nodes

--- gatsby-node.js
+++ gatsby-node.js
@@ -23,7 +23,7 @@ exports.onCreateNode = async ({
 			value: slug
 		})
 	}
-	else if (node.internal.type === 'LeaderboardUser') {
+	else if (node.internal.type === 'LeaderboardUser' || node.internal.type === 'Patron') {
 		// Create image file nodes using avatar urls, optimizes images
 		try {
 			const fileNode = await createRemoteFileNode({
@@ -81,11 +81,13 @@ exports.sourceNodes = async ({
 	createContentDigest
 }) => {
 	const { createNode } = actions
-	let data
+	let leaderboard
+	let commands
+	let patrons
 
 	if (process.env.LOOTCORD_API) {
 		try {
-			const res = await axios(
+			const lbRes = await axios(
 				{
 					method: 'POST',
 					headers: {
@@ -99,26 +101,68 @@ exports.sourceNodes = async ({
 				}
 			)
 
-			data = res.data
+			leaderboard = lbRes.data
+		}
+		catch (err) {
+			// continue
+		}
+
+		try {
+			const commandsRes = await axios(
+				{
+					method: 'POST',
+					headers: {
+						'Authorization': process.env.LOOTCORD_API_AUTH,
+						'Content-Type': 'application/json'
+					},
+					url: `${process.env.LOOTCORD_API}/api/commands`
+				},
+				{
+					timeout: 5000
+				}
+			)
+
+			commands = commandsRes.data
+		}
+		catch (err) {
+			// continue
+		}
+
+		try {
+			const patronsRes = await axios(
+				{
+					method: 'POST',
+					headers: {
+						'Authorization': process.env.LOOTCORD_API_AUTH,
+						'Content-Type': 'application/json'
+					},
+					url: `${process.env.LOOTCORD_API}/api/patrons`
+				},
+				{
+					timeout: 5000
+				}
+			)
+
+			patrons = patronsRes.data
 		}
 		catch (err) {
 			// continue
 		}
 	}
 
-	if (data) {
+	if (leaderboard) {
 		// Create LeaderboardUser nodes using leaderboard data
 		let type
 		let user
-		for (type in data) {
-			for (user in data[type]) {
+		for (type in leaderboard) {
+			for (user in leaderboard[type]) {
 				const lbData = {
 					user,
-					value: data[type][user].data.toString(),
+					value: leaderboard[type][user].data.toString(),
 
 					// TODO fix the lootcord api to NOT return formatted data so I dont have to do this...
-					rawValue: parseInt(data[type][user].data.toString().replace(/,/g, '')),
-					avatar: data[type][user].avatar
+					rawValue: parseInt(leaderboard[type][user].data.toString().replace(/,/g, '')),
+					avatar: leaderboard[type][user].avatar
 				}
 
 				try {
@@ -154,6 +198,70 @@ exports.sourceNodes = async ({
 			}
 		}
 	}
+
+	if (commands) {
+		let command
+		for (command of commands.filter(cmd => cmd.category !== 'admin' && cmd.category !== 'moderation')) {
+			const nodeContent = JSON.stringify(command)
+
+			const cmdNode = {
+				id: createNodeId(command.command),
+				parent: null,
+				children: [],
+				internal: {
+					type: 'Command',
+					contentDigest: createContentDigest(nodeContent)
+				},
+				name: command.command,
+				patronOnly: command.patronOnly,
+				description: command.shortDesc,
+				category: command.category,
+				usage: command.usage
+			}
+
+			createNode(cmdNode)
+		}
+	}
+
+	if (patrons) {
+		let user
+		for (user in patrons) {
+			const patronData = {
+				user,
+				tier: patrons[user].tier,
+				avatar: patrons[user].avatar
+			}
+
+			try {
+				await axios.get(patronData.avatar)
+			}
+			catch (err) {
+				// avatar must have been changed recently and needs to be cached again...
+
+				// use default avatar as placeholder
+				patronData.avatar = 'https://cdn.discordapp.com/embed/avatars/0.png'
+			}
+
+			const nodeContent = JSON.stringify(patronData)
+
+			const patronNode = {
+				id: createNodeId(`${user}-${patronData.tier}`),
+				parent: null,
+				children: [],
+				internal: {
+					type: 'Patron',
+					contentDigest: createContentDigest(nodeContent)
+				},
+				user: {
+					tag: user,
+					avatarURL: patronData.avatar
+				},
+				tier: patronData.tier
+			}
+
+			createNode(patronNode)
+		}
+	}
 }
 
 // graphql schema definitions to prevent errors when no LeaderboardUser nodes are created (this can happen if there's no response from the lootcord api)
@@ -170,6 +278,17 @@ exports.createSchemaCustomization = ({ actions }) => {
 			user: User
 			value: String!
 			rawValue: Int
+		}
+		type Command implements Node {
+			name: String!
+			patronOnly: Boolean
+			description: String!
+			category: String!
+			usage: String!
+		}
+		type Patron implements Node {
+			user: User
+			tier: Int
 		}
 	`
 
