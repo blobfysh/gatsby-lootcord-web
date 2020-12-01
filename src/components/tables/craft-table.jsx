568fa7919ce9d4e9af2c3fc829fe75e491d4e9b1
blobfysh <47480729+blobfysh@users.noreply.github.com>
2020-12-01 09:53:08 -0500

    table accessibility

--- src/components/tables/craft-table.jsx
+++ src/components/tables/craft-table.jsx
@@ -7,37 +7,41 @@ import styles from './table.module.scss'
 function CraftTable({ items }) {
 	return (
 		<table className={styles.table}>
-			<tr>
-				<th>Material</th>
-				<th>Amount</th>
-			</tr>
-			{items.map(material => (
-				<tr key={material.item.name}>
-					<td>
-						<div className={styles.itemCell}>
-							<div className={styles.ammoImage}>
-								{
-									material.item.image.extension !== 'gif' ?
-										<Img
-											fluid={material.item.image.childImageSharp.fluid}
-											alt=''
-											draggable={false}
-										/> :
-										<img
-											src={material.item.image.publicURL}
-											alt=''
-											draggable='false'
-										/>
-								}
-							</div>
-							<Link to={`/item/${material.item.name}`}>
-								{material.item.name}
-							</Link>
-						</div>
-					</td>
-					<td>{material.amount}x</td>
+			<thead>
+				<tr>
+					<th>Material</th>
+					<th>Amount</th>
 				</tr>
-			))}
+			</thead>
+			<tbody>
+				{items.map(material => (
+					<tr key={material.item.name}>
+						<td>
+							<div className={styles.itemCell}>
+								<div className={styles.ammoImage}>
+									{
+										material.item.image.extension !== 'gif' ?
+											<Img
+												fluid={material.item.image.childImageSharp.fluid}
+												alt=''
+												draggable={false}
+											/> :
+											<img
+												src={material.item.image.publicURL}
+												alt=''
+												draggable='false'
+											/>
+									}
+								</div>
+								<Link to={`/item/${material.item.name}`}>
+									{material.item.name}
+								</Link>
+							</div>
+						</td>
+						<td>{material.amount}x</td>
+					</tr>
+				))}
+			</tbody>
 		</table>
 	)
 }
